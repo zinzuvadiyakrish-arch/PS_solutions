d30f1142b6c74e9606be68c7aa18eea953e6d571
zinzuvadiyakrish-arch <zinzuvadiyakrish@gmail.com>
2025-09-01 09:52:20 +0530

    Update decoder.cpp

--- decoder.cpp
+++ decoder.cpp
@@ -2,33 +2,35 @@
 int main() {
     char letter;
     int i = 0;
+    int j ;
     std::string statement;
     std::cout<<"Please enter a line of text : ";
     std::getline(std::cin, statement);
+
+    std::cout<<"Please enter the shift in alphabets : ";
+    std::cin>>j;
     while (i<statement.length()) {
+
         letter = statement.at(i);
-        i=i+1;
-        if ((int(letter) > 64 && int(letter) < 91) ||
-            (int(letter) > 96 && int(letter) < 123)) {
-            if (int(letter) == 97 ) {
-                std::cout<<"y";
-            }
-            else if (int(letter) == 98 ) {
-                std::cout<<"z";
-            }
-            else if (int(letter) == 65) {
-                std::cout<<"Y";
-            }
-            else if (int(letter) == 66 ) {
-                std::cout<<"Z";
-            }
-            else {
-                letter = letter - 2;
-                std::cout<<char(letter);
-            }
-            }
-        else {
-            std::cout<<letter;
+        if(int(letter)-j<97 && int(letter)>96){
+            letter=letter-j+26;
+            std::cout<<char(letter);
         }
+
+        else if (int (letter)==32) {
+            std::cout<<char(letter);
+        }
+
+        else if (int(letter)-j<65 ) {
+            letter=letter-j+26;
+            std::cout<<char(letter);
+        }
+
+
+        else {
+            letter = letter-j;
+            std::cout<<char(letter);
+        }
+        i=i+1;
     }
 }
